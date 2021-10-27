e4f5d32a561e02383e53dae6b140e883d7a557a3
Albertojserr <alberto.j.serrano@gmail.com>
2021-10-27 23:04:47 +0200

    ejercicio 8 final

--- EJERCICIO UN JUEGO DE ADIVINAR.py
+++ EJERCICIO UN JUEGO DE ADIVINAR.py
@@ -1,11 +1,28 @@
 from random import randint
-
+print("Ejercicio entre 1 y 15")
 numero=randint( 1, 15)
-a= int(input())
-while a!=numero:
-    if(a<numero):
+minumero= int(input("Escribe un número:"))
+while minumero!=numero:
+    if(minumero<numero):
         print("Has escrito un número menor")
-    elif(a>numero):
+    elif(minumero>numero):
         print("Has escrito un número mayor")
-    a= int(input())
+    minumero= int(input("Escribe otro número:"))
 print("Has acertado el número")
+
+print("Ejercicio entre 1 y 300")
+intentos=0
+numero=randint(1,300)
+minumero=0
+while (intentos<9 and minumero!=numero):
+    minumero= int(input("Escribe un número:"))
+    if(minumero<numero):
+        print("Has escrito un número menor")
+    elif(minumero>numero):
+        print("Has escrito un número mayor")
+    intentos+=1
+
+if(minumero==numero):
+    print("Has acertado el número")
+else:
+    print("Te has pasado de intentos, el número era", numero)
